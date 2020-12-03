cd7e121feda4a96d3917ff45f2d2637a8fb5b752
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2020-12-03 03:32:31 -0500

    established an ongoing question for prompting the mac address

--- node/transmitter.ts
+++ node/transmitter.ts
@@ -5,11 +5,21 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question('Mac address to include in the magic packet?', (mac) => {
-  console.log(`The given MAC address was ${mac}`);
-});
-
 rl.on('SIGINT', () => {
   rl.close();
   process.exit();
 });
+
+const readLineConstantly = () => {
+  rl.question('Mac address to include in the magic packet?\n', (mac) => {
+    if (mac === 'exit') {
+      rl.close();
+      process.exit();
+    }
+
+    console.log(`The given MAC address was ${mac}`);
+    readLineConstantly();
+  });
+};
+
+readLineConstantly();
